cd786513008c8eba02cd75540a9b7ee0e3629f6e
kuldeepperumalla <kuldeepperumalla325@gmail.com>
2023-02-12 14:22:43 -0500

    commit #3 rendering books list

--- src/App.js
+++ src/App.js
@@ -6,10 +6,21 @@ function App(){
 
     const createBook= (title) =>{
         console.log('this is the title of the book : ',title);
+        const updatedBooks = [...books, title];
+        setBooks(updatedBooks)
     }
 
-    return <div>
+    const renderedBooks =  books.map((book, i) => {
+        return <li key={i}>{book}</li>
+    })
+
+    return (
+      <div>
         <BookCreate onCreate={createBook} />
-    </div>
+        <ul>
+          <li>{renderedBooks} </li>
+        </ul>
+      </div>
+    );
 }
 export default App
