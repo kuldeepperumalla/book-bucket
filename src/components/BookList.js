6ab744ede56bbd1194bdce72f3b2242f82d222bf
kuldeepperumalla <kuldeepperumalla325@gmail.com>
2023-02-17 10:47:45 -0500

    commit #9

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -4,7 +4,7 @@ import { useEffect, useRef } from "react";
 function BookList({ books, onDelete, onEdit}) {
   const msgEndRef = useRef(null);
   useEffect(() => {
-    msgEndRef.current?.scrollIntoView({ behavior: "smooth" });
+    msgEndRef.current?.scrollIntoView({ behavior: "smooth", bottom: '0' });
   }, [books]);
   const renderedBooks = books.map((book) => {
     return <BookShow onEdit={onEdit} onDelete={onDelete} key={book.id} book={book} />;
